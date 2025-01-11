090de5bc62548eefbae1200eedf26f106aa4876e
xmarano <leo.gregori@epitech.eu>
2025-01-12 00:09:31 +0100

    Modified Vercel domain

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 let grid = [];
 
